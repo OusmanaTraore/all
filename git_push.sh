3fe63756ae9eb58312fedc6f772c750b3629dc03
Ousmana traoré <traoreosman@yahoo.fr>
2022-04-13 13:28:11 +0100

    update readme et ajout de gitpush

---
+++ git_push.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -e "
+=================================================================================
+||||            Lister les fichier a indexer                                 ||||
+                exemple --all ou file1,file2 etc
+=================================================================================
+"
+read file_to_add
+git add $file_to_add
+
+git status
+sleep 2
+echo -e "
+=================================================================================
+||||            Entrez votre message de commit                               ||||
+                
+=================================================================================
+"
+read message_to_commit
+git commit $message_to_commit
+
+git status
+sleep 2
+
+echo -e "
+=================================================================================
+||||     Entrez votre branche sur laquelle vous pushez  <branch-projet>      ||||
+                     expemple : origin, master 
+=================================================================================
+"
+read  branch_to_push
+git push $branch_to_push
